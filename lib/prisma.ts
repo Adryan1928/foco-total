8f378db39535af21150e7e3f67ea6d45c6199e5a
Adryan Eryk de Oliveira Leite <adryanoliveira1928@gmail.com>
2025-11-16 21:18:55 -0300

    Novo teste do deploy usando a documentação do neon

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,24 @@
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
 
-export const prisma = new PrismaClient({
-    log: ["query", "error", "warn"],
-  }).$extends(withAccelerate());
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+// Type definitions
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
